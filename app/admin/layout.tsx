b48d233d2b701a0d2d320f3f405697fcefa57c80
amboseshem <amboseshem@gmail.com>
2026-09-16 18:54:42 +0300

    Load page manager styling

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,5 +1,6 @@
 import "./admin.css";
 import "./admin-live.css";
 import "./admin-extra.css";
+import "./page-manager.css";
 export const metadata={title:"MWONET Admin",robots:{index:false,follow:false}};
 export default function AdminRootLayout({children}:{children:React.ReactNode}){return <div className="admin-root">{children}</div>}
