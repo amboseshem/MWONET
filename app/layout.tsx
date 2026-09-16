f754b8f52c1a30223a2d20a4848c7076052eb5d4
amboseshem <amboseshem@gmail.com>
2026-09-16 12:26:19 +0300

    Load member authentication styles globally

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import "./constitution.css";
+import "./member-auth.css";
 import SiteHeader from "./components/SiteHeader";
 import SiteFooter from "./components/SiteFooter";
 
